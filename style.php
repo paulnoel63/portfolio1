ac392d94eb638118c641bd87b56fb1ea3f4a2970
paul <noelp.exprosso@gmail.com>
2019-10-27 17:42:28 +0100

    newversion

---
+++ style.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+   <head>
+     <meta charset="utf-8">
+     <link rel="stylesheet" type="text/css" href="style.css">
+     </head>
+     
+     <body>
+         <div class="footer1">
+         <title>cv</title>
+         <img src="startup.jpg.jxr" width="900px" height="700px" alt="coucou"/>
+         <h1>bientot ici mon cv</h1>
+
+</body>
+</html>
